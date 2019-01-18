c6ca2fe0ab52a215876f75c28bebf8f6f370b062
Danuminthar <arkarmtp11@gmail.com>
2019-01-18 03:02:49 +0300

    wifi enable

--- Wifi_Hotspot_Enable/wifi_hotspot_enable.py
+++ Wifi_Hotspot_Enable/wifi_hotspot_enable.py
@@ -4,6 +4,24 @@ import os
 os.system('cls')
 print("Wireless Enable")
 print()
-
-os.system("netsh wlan set hostednetwork mode=allow ssid=Arkar key=12345678")
-os.system("netsh wlan start hostednetwork")
+ssid="DanuMinthar"
+password="12345678"
+cmd="0"
+while cmd!="3":
+    print ("1-start hotspot")
+    print("2-stop")
+    print ("3-end")
+    cmd=input("Please Enter the choice(1,2,3): ")
+    if cmd=="1":
+        ssid=input("Please Enter SSID: ")
+        password=input("Enter Password: ")
+        command="netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + password
+        os.system(command)
+        os.system("netsh wlan start hostednetwork")
+    if cmd=="2":
+        os.system("netsh wlan stop hostednetwork")
+    if cmd=="3":
+        quit()
+    else:
+        print("You can't enter this letter!,Please re-enter")
+        os.system("pause")
